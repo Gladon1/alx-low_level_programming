ddb7e27d1f9dc0c01f168f3060b0d1a558c703f4
gladon1 <dongladys2@gmail.com>
2023-04-04 16:12:00 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,24 +2,16 @@
 #include <stdlib.h>
 
 /**
- * pop_listint - deletes the head node of a listint_t linked list
- * @head: pointer to head of singly linked list
+ * free_listint - frees memory of a list
+ * @head: head pointer of singly linked list
  *
- * Return: head node’s data (n)
+ * Return: No Return
  */
-int pop_listint(listint_t **head)
+void free_listint(listint_t *head)
 {
-	listint_t *tmp = *head;
-	listint_t *tmp2;
-	int n;
+	if (head == 0)
+		return;
 
-	if (head == 0 || tmp == 0)
-		return (0);
-
-	tmp2 = tmp->next;
-	n = tmp->n;
-	free(tmp);
-	*head = tmp2;
-
-	return (n);
+	free_listint(head->next);
+	free(head);
 }
